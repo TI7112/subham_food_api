8a5eecd178fd52e9460c2714af362bfb1c7acbf7
ti7112 <ti7112@outlook.com>
2023-07-29 16:24:54 +0530

    ayush

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ const mysql = require('mysql2');
 // create the connection to database
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'food_subham'
+  user: 'u161819951_subham_food_ap',
+  password: 'Admin@123',
+  database: 'u161819951_subham_food_ap'
 });
 
 module.exports = connection
